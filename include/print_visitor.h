1da2cdbc15a4bbe23a6783f87a54bd1b0ce8f517
Kai Gibson <kai@Admins-MacBook-Pro.local>
2026-06-22 23:20:13 +1000

    Print visitor formatting

--- include/print_visitor.h
+++ include/print_visitor.h
@@ -1,21 +1,31 @@
 #ifndef PRINT_VISITOR_H
 #define PRINT_VISITOR_H
 #include <iostream>
+#include <string>
 
 #include "ast.h"
 #include "visitor.h"
 
 struct PrintVisitor : public Visitor {
+  std::size_t indent = 0;
+
+  std::string pad() { return std::string(indent * 2, ' '); }
+
   void visit(FloatLiteralExpression& expr) override {
     std::cout << "FloatLiteral(" << expr.value << ")";
   }
 
   void visit(BinaryExpression& expr) override {
-    std::cout << "BinaryExpression(" << token_type_to_str(expr.op) << ", Lhs(";
+    std::cout << "BinaryExpression(" << token_type_to_str(expr.op) << "\n";
+    indent++;
+    std::cout << pad() << "Lhs(";
     expr.lhs->accept(*this);
-    std::cout << "), Rhs(";
+    std::cout << ")\n";
+    std::cout << pad() << "Rhs(";
     expr.rhs->accept(*this);
     std::cout << ")\n";
+    indent--;
+    std::cout << pad() << ")";
   }
 
   void visit(VariableExpression& expr) override {
@@ -23,52 +33,58 @@ struct PrintVisitor : public Visitor {
   }
 
   void visit(VariableDeclarationStatement& stmt) override {
-    std::cout << "VariableDeclarationStatement(\n\t\tName(\"" << stmt.name
-              << "\"),\n\t\tValue(";
+    std::cout << "VariableDeclarationStatement(\"" << stmt.name << "\"\n";
+    indent++;
+    std::cout << pad() << "Value(";
     stmt.value->accept(*this);
-
+    std::cout << ")";
+    indent--;
     std::cout << ")";
   }
 
   void visit(VariableAssignmentStatement& stmt) override {
-    std::cout << "VariableAssignmentStatement(\n\t\tName(\"" << stmt.name
-              << "\"),\n\t\tValue(";
+    std::cout << "VarAssign(\"" << stmt.name << "\"\n";
+    indent++;
+    std::cout << pad() << "Value(";
     stmt.value->accept(*this);
-
     std::cout << ")";
-  }
-
-  void visit(Program& program) override {
-    std::cout << "Program(\n";
-    for (const auto& func : program.functions) {
-      std::cout << "\t";
-      func->accept(*this);
-      std::cout << ",\n";
-    }
-
+    indent--;
     std::cout << ")";
   }
 
   void visit(ShowStatement& stmt) override {
-    std::cout << "Show(\n";
+    std::cout << "Show(";
     stmt.expr->accept(*this);
     std::cout << ")";
   }
 
   void visit(FunctionDeclaration& func) override {
-    std::cout << "FunctionDeclaration(\n\tName(\"" << func.name << "\"\n";
-
-    for (const auto& statement : func.statements) {
-      std::cout << "\t";
-      statement->accept(*this);
-      std::cout << ",\n";
+    std::cout << "FunctionDeclaration(\n";  // removed pad() here
+    indent++;
+    std::cout << pad() << "Name(\"" << func.name << "\")\n";
+    for (const auto& stmt : func.statements) {
+      std::cout << pad();
+      stmt->accept(*this);
+      std::cout << "\n";
     }
-    std::cout << ")";
+    indent--;
+    std::cout << pad() << ")";
   }
 
-  void visit(FunctionCallExpression& funccall) override {
-    std::cout << "FunctionCallExpression(\n\tName(\"" << funccall.name
-              << "\"\n";
+  void visit(FunctionCallExpression& call) override {
+    std::cout << "FunctionCall(\"" << call.name << "\")";
+  }
+
+  void visit(Program& program) override {
+    std::cout << "Program(\n";
+    indent++;
+    for (const auto& func : program.functions) {
+      std::cout << pad();
+      func->accept(*this);
+      std::cout << "\n";
+    }
+    indent--;
+    std::cout << ")\n";
   }
 };
 
